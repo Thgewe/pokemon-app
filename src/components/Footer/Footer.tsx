0f7feac7efb877ec609c205a8ef0fc600b407a53
Viktor Khankhatov <hanhatov@mail.ru>
2023-11-05 00:03:28 +0800

    new & fix: footer & header

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import cl from "./footer.module.css";
+import {ReactComponent as GitHub} from "../../images/svg/github.svg";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return <footer className={cl.footer}>
+		<div className={cl.content}>
+			<h3>Developed by Thgewe</h3>
+			<a href={"https://github.com/Thgewe"} title={"My GitHub"}>
+				<GitHub />
+			</a>
+		</div>
+	</footer>;
 };
 
 export default Footer;
